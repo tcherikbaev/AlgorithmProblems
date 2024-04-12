342e30df0aed493c0fc7a4541992d0fcb5787fef
Tilek <tcherikbaev@na.edu>
2024-04-11 21:13:44 -0500

    edit array test in move zeros

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main {
         System.out.println("****************************************************");
         //283. Moves zeros
         System.out.println("Problem #283 'Move Zeros' : ");
-        int[] nums2={3,43,0,54,5,8,0,4,0,12,13};
+        int[] nums2={3,43,0,54,5,8,0,4,0,12,13,0,0,44,33,22,11};
         MoveZeros.MoveZeroNumber(nums2);
         for(int i:nums2)
         {
