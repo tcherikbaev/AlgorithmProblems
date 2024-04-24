0b90ae184e7b4d639012e511c37a96ad5e0752c6
Tilek <tcherikbaev@na.edu>
2024-04-23 21:06:44 -0700

    Sort colors

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,7 @@ public class Main {
 
         String word="a good   example for";
         String reverse= ReverseString.reverseWords(word);
-        System.out.println("Problem #151 'Reverse Words in a Strings Java' : ");
+        System.out.println("Problem #151 'Reverse Words in a Strings Java Code' : ");
         System.out.println(reverse);
         //75. Sort Colors
         int[] numsArr = {0, 0, 2, 1, 1,2,1,0,2,0,0};
