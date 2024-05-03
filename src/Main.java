311d762a5f507f7ca37d84abcd9f480f5ed2f682
Tilek <tcherikbaev@na.edu>
2024-05-03 00:38:57 -0500

    Move Zeros problem

--- src/Main.java
+++ src/Main.java
@@ -10,15 +10,6 @@ public class Main {
         int result = SingleNumber.SingleNum(nums);
         System.out.println("Problem #136 'Single Number' : " + result);
         System.out.println("****************************************************");
-        //283. Moves zeros
-        System.out.println("Problem #283 'Move Zeros' : ");
-        int[] nums2={3,43,0,54,5,8,0,4,0,12,13,0,0,44,33,22,11};
-        MoveZeros.MoveZeroNumber(nums2);
-        for(int i:nums2)
-        {
-            System.out.print(i +" ");
-        }
-        System.out.println("****************************************************");
         //151. Reverse Words in a String
 
         String word="a good   example for";
--- src/MoveZeros.java
+++ src/MoveZeros.java
@@ -1,4 +1,17 @@
 public class MoveZeros {
+
+    public static void main (String []  args)
+    {
+
+        //283. Moves zeros
+        System.out.println("Problem #283 'Move Zeros' : ");
+        int[] nums2={3,43,0,54,5,8,0,4,0,12,13,0,0,44,33,22,11};
+        MoveZeros.MoveZeroNumber(nums2);
+        for(int i:nums2)
+        {
+            System.out.print(i +" ");
+        }
+    }
     public static void MoveZeroNumber(int[] nums)
     {
         int j=0;
