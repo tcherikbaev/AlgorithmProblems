212d2b9f3591c8bb9426ee61e2356eacb79617a4
Tilek <tcherikbaev@na.edu>
2024-05-03 00:37:07 -0500

    Missing Number problem

--- src/MissingNumber.java
+++ src/MissingNumber.java
@@ -1,5 +1,15 @@
 public class MissingNumber {
-   static public int missingNum(int[] nums) {
+
+    public static void main (String []  args)
+    {
+        //268. Missing Number
+        int[] missArr = {9,6,4,2,3,5,7,0,1};
+        int missNumber=MissingNumber.missingNum(missArr);
+        System.out.println("268. Missing Number' : ");
+        System.out.println("Miss Number: " +missNumber);
+    }
+
+        static public int missingNum(int[] nums) {
         int n = nums.length;
 
         int expectedSum = n * (n + 1) / 2;
