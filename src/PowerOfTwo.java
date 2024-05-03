87f9572412e7eb20aa29764b912e5cdba74e3906
Tilek <tcherikbaev@na.edu>
2024-05-02 19:49:14 -0500

    231. Problem , two solutions

--- src/PowerOfTwo.java
+++ src/PowerOfTwo.java
@@ -3,8 +3,10 @@ public class PowerOfTwo {
     public static void main (String []  args)
     {
         System.out.println("231. problem: ");
-       System.out.println("if n=45: "+isPowerOfTwo(64));
+        int n=64;
+       System.out.println("Bitwise : "+isPowerOfTwo(n));
 
+        System.out.println("Recursion : "+isPowerOfTwo2(n));
     }
 
     //Solution using Bitwise operation
@@ -14,6 +16,18 @@ public class PowerOfTwo {
             return false;
         }
         return (n&(n-1))==0;
+    }
+    private static boolean isPowerOfTwo2(int n)
+    {
+        if(n<=0)
+        {
+            return false;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        return (n%2==0) && isPowerOfTwo2(n/2);
     }
 
 
