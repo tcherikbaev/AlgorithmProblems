b211e9b814ea447622d8cad27aa9d7c168a01612
Tilek <tcherikbaev@na.edu>
2024-05-21 19:08:03 -0500

    hasCycle step2

--- src/CycleList.java
+++ src/CycleList.java
@@ -9,12 +9,18 @@ public class CycleList {
       }
       public static void main(String []strings)
       {
-          ListNode node=new ListNode(1);
-
+          ListNode head1;
+          head1 = new ListNode(1);
+          head1.next = new ListNode(2);
+          head1.next.next = new ListNode(3);
+          head1.next.next.next = new ListNode(4);
+          head1.next.next.next.next = new ListNode(5);
+          boolean hasCycle1 = hasCycle(head1);
+          System.out.println("Linked list 1 has cycle: " + hasCycle1);
 
 
       }
-    public boolean hasCycle(ListNode head) {
+    public static boolean hasCycle(ListNode head) {
         ListNode slow=head;
         ListNode fast=head;
         while (fast!=null && fast.next!=null)
