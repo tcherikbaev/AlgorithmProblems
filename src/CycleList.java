bc865467dacc9f1f50796d5d7c47bde434d58324
Tilek <tcherikbaev@na.edu>
2024-05-22 17:53:32 -0500

    remove elements in Singly List(by value parameter)

--- src/CycleList.java
+++ src/CycleList.java
@@ -42,6 +42,23 @@ public class CycleList {
         }
         return false;
     }
-
+    public static ListNode removeElements(ListNode head,int val)
+    {
+      while(head!=null && head.val==val)
+      {
+          head=head.next;
+      }
+      ListNode current=head;
+      ListNode prev=null;
+      while(current!=null) {
+          if (current.val == val) {
+              prev.next = current.next;
+          } else {
+              prev = current;
+          }
+          current = current.next;
+      }
+      return head;
+    }
     }
 
