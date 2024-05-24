5fc80434b0b410f7cb326fce61adf31a8edeecc3
Tilek <tcherikbaev@na.edu>
2024-05-23 19:47:15 -0500

    add Rotate Right method

--- src/CycleList.java
+++ src/CycleList.java
@@ -83,5 +83,28 @@ public class CycleList {
         }
         return head;
     }
+
+    public static ListNode rotateRight(ListNode head,int k)
+    {
+        if (head == null || head.next==null|| k==0) {
+            return head;
+        }
+        ListNode current=head;
+        int length=1;
+        while(current.next!=null)
+        {
+            current=current.next;
+            length++;
+        }
+        current.next=head;
+        int rotateIndex=k%length;
+        for(int i=0;i<length-rotateIndex-1;i++)
+        {
+            head=head.next;
+        }
+        ListNode newNode=head.next;
+        head.next=null;
+        return newNode;
+    }
         }
 
