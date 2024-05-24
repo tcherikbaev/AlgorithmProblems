56c18bd4a29c6ccd238e0e86a791fc13b1b26fb6
Tilek <tcherikbaev@na.edu>
2024-05-24 17:28:04 -0500

    addEvenList

--- src/CycleList.java
+++ src/CycleList.java
@@ -141,4 +141,23 @@ public class CycleList {
         node.val = node.next.val;
         node.next = node.next.next;
     }
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null || head.next==null) return head;
+        ListNode odd=head;
+        ListNode even=head.next;
+        ListNode evenHead=even;
+
+        while (even!=null && even.next!=null) {
+            odd.next=even.next;
+            odd=odd.next;
+            even.next=odd.next;
+            even=even.next;
+        }
+        odd.next=evenHead;
+
+        return head;
+
+    }
+
+    }
 }
